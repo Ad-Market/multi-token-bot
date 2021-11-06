923227c6806de9402668c0d8960b327b1ef52b50
heycryptobob <heycryptobob@gmail.com>
2021-11-06 17:31:53 +0100

    start after setup

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-import dotenv from 'dotenv'
-import { setupClient } from './tg.js'
-import { getLatest } from './dex.js'
-import { format } from './message.js'
-import express from 'express'
+import dotenv from "dotenv";
+import { setupClient } from "./tg.js";
+import { getLatest } from "./dex.js";
+import { format } from "./message.js";
+import express from "express";
 
-dotenv.config()
-const env = process.env
-const app = express()
+dotenv.config();
+const env = process.env;
+const app = express();
 
-const client = await setupClient()
-client.setParseMode("html")
-
-setInterval(async () => {
-  const latest = await getLatest()
-  const message = format(latest)
-  await client.sendMessage(env.CHANNEL_NAME, { message })
-  console.log('Message sent.')
-}, env.INTERVAL)
-
-app.get('/', (req, res) => {
-  res.send('alive')
-})
+app.get("/", (req, res) => {
+  res.send("alive");
+});
 
 app.listen(env.PORT, () => {
-  console.log(`App running on port ${env.PORT}`)
-})
+  console.log(`App running on port ${env.PORT}`);
+
+  const client = await setupClient();
+  client.setParseMode("html");
+
+  setInterval(async () => {
+    const latest = await getLatest();
+    const message = format(latest);
+    await client.sendMessage(env.CHANNEL_NAME, { message });
+    console.log("Message sent.");
+  }, env.INTERVAL);
+});
