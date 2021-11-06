d99a161534e4966f0e00322457cc358a911c1a5a
heycryptobob <heycryptobob@gmail.com>
2021-11-06 17:29:21 +0100

    added Express

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import dotenv from 'dotenv'
 import { setupClient } from './tg.js'
 import { getLatest } from './dex.js'
 import { format } from './message.js'
+import express from 'express'
 
 dotenv.config()
 const env = process.env
+const app = express()
 
 const client = await setupClient()
 client.setParseMode("html")
@@ -15,3 +17,11 @@ setInterval(async () => {
   await client.sendMessage(env.CHANNEL_NAME, { message })
   console.log('Message sent.')
 }, env.INTERVAL)
+
+app.get('/', (req, res) => {
+  res.send('alive')
+})
+
+app.listen(env.PORT, () => {
+  console.log(`App running on port ${env.PORT}`)
+})
